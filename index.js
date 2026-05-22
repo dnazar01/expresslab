8e2aa714c5631ce3c5ce6fff41bdaf6282976e84
dnazar01 <76090939+dnazar01@users.noreply.github.com>
2026-05-23 01:51:10 +0300

    Initialize Express application in index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createReadStream } from 'node:fs';
+import crypto from 'node:crypto';
+import http from 'node:http';
+import appSrc from './app.js';
+
+const app = appSrc(
+    express,
+    bodyParser,
+    createReadStream,
+    crypto,
+    http
+);
+
+app.listen(process.env.PORT);
