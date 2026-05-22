bb0aba61dc451e90ffa97e97bb102155c8a68ecb
dnazar01 <76090939+dnazar01@users.noreply.github.com>
2026-05-23 02:07:12 +0300

    Fix import statement for appSrc in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { createReadStream } from 'node:fs';
 import crypto from 'node:crypto';
 import http from 'node:http';
-import {appSrc} from './app.js';
+import appSrc from './app.js';
 
 const app = appSrc(
     express,
